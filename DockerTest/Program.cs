2d6f7cce0520ceaa429af8e4b916b76123e001ea
Adam Kallenbach <adamkallenbach@gmail.com>
2018-03-21 14:44:40 -0500

    Add project files.

---
+++ DockerTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DockerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
